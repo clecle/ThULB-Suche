e71fcaf5403b3b894ff91a2bf0d1708c8dd7113a
Richard Großer <richard.grosser@thulb.uni-jena.de>
2018-01-10 09:45:34 +0100

    Add 'vufind/themes/thulb/' from commit '433fc160eba63705385877f43158d1d5bc7dcd1b'
    git-subtree-dir: vufind/themes/thulb
    git-subtree-mainline: eb839e1276afe1e5ecffbaed3c2058cfe1a038b0
    git-subtree-split: 433fc160eba63705385877f43158d1d5bc7dcd1b

---
+++ vufind/themes/thulb/theme.config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    'extends' => 'bootstrap3',
+    'helpers' => [
+        'factories' => [
+            'recorddataformatter' => 'ThULB\View\Helper\Root\RecordDataFormatterFactory',
+            'recordlink' => 'ThULB\View\Helper\Root\Factory::getRecordLink',
+            'record' => 'ThULB\View\Helper\Root\Factory::getRecord',
+        ],
+    ],
+    'favicon' => 'thulb_favicon.ico',
+    'js' => array(
+            'thulb.js'
+        ),
+);

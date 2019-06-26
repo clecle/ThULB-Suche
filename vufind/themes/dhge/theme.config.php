c265436ccdf5300e087a7646b75329860051b0d2
shkgrto <tobiasgroth@gmx.de>
2019-06-26 16:12:35 +0200

    Fixed dhge theme to work with VuFind 5.1.1

--- vufind/themes/dhge/theme.config.php
+++ vufind/themes/dhge/theme.config.php
@@ -1,12 +1,5 @@
 <?php
 return array(
     'extends' => 'thulb',
-    'helpers' => [
-        'factories' => [
-            'recorddataformatter' => 'ThULB\View\Helper\Root\RecordDataFormatterFactory',
-            'recordlink' => 'ThULB\View\Helper\Root\Factory::getRecordLink',
-            'record' => 'ThULB\View\Helper\Root\Factory::getRecord',
-        ],
-    ],
     'favicon' => 'dhge_favicon.ico',
 );

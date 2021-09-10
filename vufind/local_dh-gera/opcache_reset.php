bf7c842250d31eb2f5a25861471cef3be3ce3cfb
Tobias Groth <tobias.groth@uni-jena.de>
2021-09-10 08:54:02 +0200

    DHGE: Renamed config folder 'local_dh-gera' to 'local_dhge'. Changed route to local dhge instance from 'dh-gera' to 'dhge'. refs #483

--- vufind/local_dh-gera/opcache_reset.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
-    opcache_reset();
-    echo json_encode(array('success' => true));
-} else {
-    header("HTTP/1.1 401 Unauthorized");
-    die();
-}

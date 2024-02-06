4a147c822b3789e48db585ebe931d4787e0a2b91
Tobias Groth <tobias.groth@uni-jena.de>
2024-02-06 14:37:07 +0100

    Changed menu items in navbar

--- vufind/themes/thulb/js/thulb.js
+++ vufind/themes/thulb/js/thulb.js
@@ -243,6 +243,32 @@ $(document).ready(function thulbDocReady() {
             $('#cartItems').click();
         }
     });
+
+    // enable submenus
+    $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+        // Avoid following the href location when clicking
+        event.preventDefault();
+        // Avoid having the menu to close when clicking
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+
+        // If a menu is already open we close it
+        $('ul.dropdown-menu [data-toggle=dropdown]').parent().removeClass('open');
+        // opening the one you clicked on
+        $(this).parent().addClass('open');
+
+        let menu = $(this).parent().find("ul");
+        let menuPos = menu.offset();
+
+        let newPos;
+        if ((menuPos.left + menu.width()) + 30 > $(window).width()) {
+            newPos = - menu.width();
+        }
+        else {
+            newPos = $(this).parent().width();
+        }
+        menu.css({ left:newPos });
+    });
 });
 
 document.addEventListener('VuFind.lightbox.rendered', function(event) {

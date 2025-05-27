2d95b60da3a5e34ef11595b4df3452c76e4cd603
Tobias Groth <tobias.groth@uni-jena.de>
2025-05-27 10:22:04 +0200

    Updated free ordering confirmation mail. Fixed outlook removing line breaks.

--- vufind/module/ThULB/src/ThULB/Mailer/Mailer.php
+++ vufind/module/ThULB/src/ThULB/Mailer/Mailer.php
@@ -3,6 +3,9 @@
 namespace ThULB\Mailer;
 
 use Interop\Container\ContainerInterface;
+use Laminas\Mime\Message;
+use Laminas\Mime\Message as MimeMessage;
+use Laminas\Mime\Mime;
 use VuFind\Exception\Mail as MailException;
 use VuFind\Mailer\Mailer as OriginalMailer;
 use VuFind\RecordDriver\AbstractBase;
@@ -14,6 +17,50 @@ class Mailer extends OriginalMailer {
 
     private ?string $defaultReplyTo = null;
     private ?ContainerInterface $serviceLocator = null;
+
+    protected function addTrailingSpaces (string $string): string {
+        return preg_replace("/(?<!\t)((?<!\r)(?=\n)|(?=\r\n))/m", "   $1", $string);
+    }
+
+    /**
+     * Send an email message.
+     *
+     * @param string|Address|AddressList $to      Recipient email address (or
+     * delimited list)
+     * @param string|Address             $from    Sender name and email address
+     * @param string                     $subject Subject line for message
+     * @param string|MimeMessage         $body    Message body
+     * @param string                     $cc      CC recipient (null for none)
+     * @param string|Address|AddressList $replyTo Reply-To address (or delimited
+     * list, null for none)
+     *
+     * @throws MailException
+     * @return void
+     */
+    public function send($to, $from, $subject, $body, $cc = null, $replyTo = null) {
+        // add three trailing spaces to lines in text emails to prevent outlook from removing linebreaks. see
+        // https://stackoverflow.com/questions/136052/how-do-i-format-a-string-in-an-email-so-outlook-will-print-the-line-breaks
+        // https://stackoverflow.com/questions/247546/outlook-autocleaning-my-line-breaks-and-screwing-up-my-email-format
+
+        if ($body instanceof MimeMessage) {
+            $parts = $body->getParts();
+            $body = new Message();
+
+            foreach ($parts as $part) {
+                if($part->getType() === Mime::TYPE_TEXT) {
+                    $part->setContent(
+                        $this->addTrailingSpaces($part->getContent())
+                    );
+                }
+                $body->addPart($part);
+            }
+        }
+        elseif (is_string($body)) {
+            $body = $this->addTrailingSpaces($body);
+        }
+
+        parent::send($to, $from, $subject, $body, $cc, $replyTo);
+    }
 
     /**
      * Send an email message representing a link.
